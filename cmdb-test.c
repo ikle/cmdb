e5cf212664df35b658a767d3d9b0b8bc77127b1a
Alexei A. Smekalkine <ikle@ikle.ru>
2019-07-01 14:11:17 +0300

    cmdb-test: use new utility function, reduce code dublication

--- cmdb-test.c
+++ cmdb-test.c
@@ -12,6 +12,7 @@
 #include <err.h>
 
 #include "cmdb.h"
+#include "cmdb-util.h"
 
 static void show_attr (struct cmdb *o, int level, const char *name)
 {
@@ -35,44 +36,6 @@ static void show (struct cmdb *o, int level)
 			show (o, level + 1);
 			cmdb_pop (o);
 		}
-	}
-}
-
-static void show_attr_sorted (struct cmdb *o, int level, const char *name)
-{
-	const char **list, **p;
-
-	if ((list = cmdb_list (o, name)) == NULL)
-		return;
-
-	for (p = list; *p != NULL; ++p)
-		printf ("%*s%s = %s\n", level * 4, "", name, *p);
-
-	free (list);
-}
-
-static void show_sorted (struct cmdb *o, int level)
-{
-	const char **list, **p;
-
-	if ((list = cmdb_list (o, "\a")) != NULL) {
-		for (p = list; *p != NULL; ++p)
-			show_attr_sorted (o, level, *p);
-
-		free (list);
-	}
-
-	if ((list = cmdb_list (o, "\n")) != NULL) {
-		for (p = list; *p != NULL; ++p) {
-			printf ("%*s%s:\n", level * 4, "", *p);
-
-			if (cmdb_push (o, *p)) {
-				show_sorted (o, level + 1);
-				cmdb_pop (o);
-			}
-		}
-
-		free (list);
 	}
 }
 
@@ -120,7 +83,7 @@ int main (int argc, char *argv[])
 		errx (1, "cannot set level");
 
 	printf ("\n");
-	show_sorted (o, 0);
+	cmdb_save (o, stdout);
 	cmdb_close (o);
 	return 0;
 }
